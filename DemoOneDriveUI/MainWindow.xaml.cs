29e78503fcc2191d63454ff71a50be365fcbc571
Olubunmi Osuntuyi <Olubunmi.Osuntuyi@advantech.com>
2021-11-25 01:54:23 +0100

    [UPDATE] Added button click functionality for controls in footer

--- DemoOneDriveUI/MainWindow.xaml.cs
+++ DemoOneDriveUI/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,5 +31,31 @@ namespace DemoOneDriveUI
         {
 
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (combobox.Visibility == Visibility.Visible)
+            {
+                combobox.Visibility = Visibility.Hidden;
+                combobox.IsDropDownOpen = false;         
+            }
+            else 
+            {
+                combobox.Visibility = Visibility.Visible;
+                combobox.IsDropDownOpen = true;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://www.microsoft.com/en-us/microsoft-365/onedrive/online-cloud-storage");
+ 
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Process.Start(@"C:\Users");
+        }
+
     }
 }
